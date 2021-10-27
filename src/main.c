48629f2d409c003635738ca93996f4d61856e9f4
Kurtis Dinelle <kurtisdinelle@gmail.com>
2021-10-27 12:22:58 -0700

    Complete all non-display and key related instructions

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <time.h>
 
-bool load_rom(char *filename, unsigned char memory[])
+bool load_rom(char *filename, unsigned char RAM[])
 {
     /* Loads a given ROM into memory starting at address 0x200.
     0x200 is where user data is stored, eeverything before that is system. */
@@ -11,7 +11,7 @@ bool load_rom(char *filename, unsigned char memory[])
     FILE *rom = fopen(filename, "rb");
     if (rom)
     {
-        fread(memory + 0x200, 4096 - 0x200, 1, rom);
+        fread(RAM + 0x200, 4096 - 0x200, 1, rom);
         fclose(rom);
 
         return true;
@@ -22,108 +22,108 @@ bool load_rom(char *filename, unsigned char memory[])
     }
 }
 
-void load_font(unsigned char memory[])
+void load_font(unsigned char RAM[])
 {
     /* Load hexadecimal font into addresses 0x000-0x04F.
     Each hex character is represented by 5 bytes in memory
     with each bit representing a pixel. */
 
     // 0:
-    memory[0x0] = 0xF0;
-    memory[0x1] = 0x90;
-    memory[0x2] = 0x90;
-    memory[0x3] = 0x90;
-    memory[0x4] = 0xF0;
+    RAM[0x0] = 0xF0;
+    RAM[0x1] = 0x90;
+    RAM[0x2] = 0x90;
+    RAM[0x3] = 0x90;
+    RAM[0x4] = 0xF0;
     // 1:
-    memory[0x5] = 0x20;
-    memory[0x6] = 0x60;
-    memory[0x7] = 0x20;
-    memory[0x8] = 0x20;
-    memory[0x9] = 0x70;
+    RAM[0x5] = 0x20;
+    RAM[0x6] = 0x60;
+    RAM[0x7] = 0x20;
+    RAM[0x8] = 0x20;
+    RAM[0x9] = 0x70;
     // 2:
-    memory[0xA] = 0xF0;
-    memory[0xB] = 0x10;
-    memory[0xC] = 0xF0;
-    memory[0xD] = 0x80;
-    memory[0xE] = 0xF0;
+    RAM[0xA] = 0xF0;
+    RAM[0xB] = 0x10;
+    RAM[0xC] = 0xF0;
+    RAM[0xD] = 0x80;
+    RAM[0xE] = 0xF0;
     // 3:
-    memory[0xF] = 0xF0;
-    memory[0x10] = 0x10;
-    memory[0x11] = 0xF0;
-    memory[0x12] = 0x10;
-    memory[0x13] = 0xF0;
+    RAM[0xF] = 0xF0;
+    RAM[0x10] = 0x10;
+    RAM[0x11] = 0xF0;
+    RAM[0x12] = 0x10;
+    RAM[0x13] = 0xF0;
     // 4:
-    memory[0x14] = 0x90;
-    memory[0x15] = 0x90;
-    memory[0x16] = 0xF0;
-    memory[0x17] = 0x10;
-    memory[0x18] = 0x10;
+    RAM[0x14] = 0x90;
+    RAM[0x15] = 0x90;
+    RAM[0x16] = 0xF0;
+    RAM[0x17] = 0x10;
+    RAM[0x18] = 0x10;
     // 5:
-    memory[0x19] = 0xF0;
-    memory[0x1A] = 0x80;
-    memory[0x1B] = 0xF0;
-    memory[0x1C] = 0x10;
-    memory[0x1D] = 0xF0;
+    RAM[0x19] = 0xF0;
+    RAM[0x1A] = 0x80;
+    RAM[0x1B] = 0xF0;
+    RAM[0x1C] = 0x10;
+    RAM[0x1D] = 0xF0;
     // 6:
-    memory[0x1E] = 0xF0;
-    memory[0x1F] = 0x80;
-    memory[0x20] = 0xF0;
-    memory[0x21] = 0x90;
-    memory[0x22] = 0xF0;
+    RAM[0x1E] = 0xF0;
+    RAM[0x1F] = 0x80;
+    RAM[0x20] = 0xF0;
+    RAM[0x21] = 0x90;
+    RAM[0x22] = 0xF0;
     // 7:
-    memory[0x23] = 0xF0;
-    memory[0x24] = 0x10;
-    memory[0x25] = 0x20;
-    memory[0x26] = 0x40;
-    memory[0x27] = 0x40;
+    RAM[0x23] = 0xF0;
+    RAM[0x24] = 0x10;
+    RAM[0x25] = 0x20;
+    RAM[0x26] = 0x40;
+    RAM[0x27] = 0x40;
     // 8:
-    memory[0x28] = 0xF0;
-    memory[0x29] = 0x90;
-    memory[0x2A] = 0xF0;
-    memory[0x2B] = 0x90;
-    memory[0x2C] = 0xF0;
+    RAM[0x28] = 0xF0;
+    RAM[0x29] = 0x90;
+    RAM[0x2A] = 0xF0;
+    RAM[0x2B] = 0x90;
+    RAM[0x2C] = 0xF0;
     // 9:
-    memory[0x2D] = 0xF0;
-    memory[0x2E] = 0x90;
-    memory[0x2F] = 0xF0;
-    memory[0x30] = 0x10;
-    memory[0x31] = 0xF0;
+    RAM[0x2D] = 0xF0;
+    RAM[0x2E] = 0x90;
+    RAM[0x2F] = 0xF0;
+    RAM[0x30] = 0x10;
+    RAM[0x31] = 0xF0;
     // A:
-    memory[0x32] = 0xF0;
-    memory[0x33] = 0x90;
-    memory[0x34] = 0xF0;
-    memory[0x35] = 0x90;
-    memory[0x36] = 0x90;
+    RAM[0x32] = 0xF0;
+    RAM[0x33] = 0x90;
+    RAM[0x34] = 0xF0;
+    RAM[0x35] = 0x90;
+    RAM[0x36] = 0x90;
     // B:
-    memory[0x37] = 0xE0;
-    memory[0x38] = 0x90;
-    memory[0x39] = 0xE0;
-    memory[0x3A] = 0x90;
-    memory[0x3B] = 0xE0;
+    RAM[0x37] = 0xE0;
+    RAM[0x38] = 0x90;
+    RAM[0x39] = 0xE0;
+    RAM[0x3A] = 0x90;
+    RAM[0x3B] = 0xE0;
     // C:
-    memory[0x3C] = 0xF0;
-    memory[0x3D] = 0x80;
-    memory[0x3E] = 0x80;
-    memory[0x3F] = 0x80;
-    memory[0x40] = 0xF0;
+    RAM[0x3C] = 0xF0;
+    RAM[0x3D] = 0x80;
+    RAM[0x3E] = 0x80;
+    RAM[0x3F] = 0x80;
+    RAM[0x40] = 0xF0;
     // D:
-    memory[0x41] = 0xE0;
-    memory[0x42] = 0x90;
-    memory[0x43] = 0x90;
-    memory[0x44] = 0x90;
-    memory[0x45] = 0xE0;
+    RAM[0x41] = 0xE0;
+    RAM[0x42] = 0x90;
+    RAM[0x43] = 0x90;
+    RAM[0x44] = 0x90;
+    RAM[0x45] = 0xE0;
     // E:
-    memory[0x46] = 0xF0;
-    memory[0x47] = 0x80;
-    memory[0x48] = 0xF0;
-    memory[0x49] = 0x80;
-    memory[0x4A] = 0xF0;
+    RAM[0x46] = 0xF0;
+    RAM[0x47] = 0x80;
+    RAM[0x48] = 0xF0;
+    RAM[0x49] = 0x80;
+    RAM[0x4A] = 0xF0;
     // F:
-    memory[0x4B] = 0xF0;
-    memory[0x4C] = 0x80;
-    memory[0x4D] = 0xF0;
-    memory[0x4E] = 0x80;
-    memory[0x4F] = 0x80;
+    RAM[0x4B] = 0xF0;
+    RAM[0x4C] = 0x80;
+    RAM[0x4D] = 0xF0;
+    RAM[0x4E] = 0x80;
+    RAM[0x4F] = 0x80;
 }
 
 int main()
@@ -131,7 +131,7 @@ int main()
     srand(time(NULL));
 
     // Represents 4096 bytes (4KB) of addressable memory.
-    unsigned char memory[4096] = {0x00};
+    unsigned char RAM[4096] = {0x00};
 
     // Represents 16 general-purpose 8-bit registers (V0-VF).
     unsigned char V[16];
@@ -149,20 +149,20 @@ int main()
     A pixel can be either only on or off, no color. */
     bool display[32][64] = {false};
 
-    load_font(memory);
+    load_font(RAM);
 
     // Load ROM into memory (for now filename hardcoded).
-    if (!load_rom("../roms/my_test.c8", memory))
+    if (!load_rom("../roms/my_test.c8", RAM))
     {
         fprintf(stderr, "Unable to open ROM file.\n");
         return 1;
     }
 
-    // Execute instructions until none (0x0000) is found.
-    while (!(memory[PC] == 0x00 && memory[PC + 1] == 0x00))
+    // Read and execute instructions from memory until none (0x0000) is found.
+    while (!(RAM[PC] == 0x00 && RAM[PC + 1] == 0x00))
     {
         // The first and second byte of instruction respectively.
-        unsigned char b1 = memory[PC], b2 = memory[PC + 1];
+        unsigned char b1 = RAM[PC], b2 = RAM[PC + 1];
 
         // The code (first 4 bits) of instruction.
         unsigned char C = b1 >> 4;
@@ -176,7 +176,7 @@ int main()
         // The last 12 bits of instruction (usually an address).
         unsigned int NNN = ((b1 & 0xF) << 8) | (int)b2;
 
-        // The last 8 bits of instruction (usually an address).
+        // The last 8 bits of instruction.
         unsigned char NN = b2;
 
         // The last 4 bits of instruction.
@@ -308,7 +308,7 @@ int main()
             // SHR VX {, VY}
             case 0x06:
                 V[0xF] = V[X] & 0x01;
-                V[X] /= 2;
+                V[X] >> 1;
                 break;
 
             // SUBN VX, VY
@@ -328,7 +328,10 @@ int main()
             // SHL VX {, VY}
             case 0x0E:
                 V[0xF] = V[X] & 0x80;
-                V[X] *= 2;
+                V[X] << 1;
+                break;
+
+            default:
                 break;
             }
 
@@ -354,6 +357,99 @@ int main()
         // RND VX, NN:
         case 0x0C:
             V[X] = (rand() % 256) & NN;
+            break;
+
+        // DRW VX, VY, N:
+        case 0x0D:
+            // Do draw stuff
+            break;
+
+        // Key skip family
+        case 0x0E:
+            switch (b2)
+            {
+            // SKP VX
+            case 0x9E:
+                // Do key stuff
+                break;
+
+            // SKNP VX
+            case 0xA1:
+                // Do key stuff
+                break;
+
+            default:
+                break;
+            }
+
+            break;
+
+        // F command family
+        case 0x0F:
+            switch (b2)
+            {
+            // LD VX, DT
+            case 0x07:
+                V[X] = DT;
+                break;
+
+            // LD VX, K
+            case 0x0A:
+                // Do key stuff
+                break;
+
+            // LD DT, VX
+            case 0x15:
+                DT = V[X];
+                break;
+
+            // LD ST, VX
+            case 0x18:
+                ST = V[X];
+                break;
+
+            // ADD I, VX
+            case 0x1E:
+                I += V[X];
+                break;
+
+            // LD F, VX
+            case 0x29:
+                I = V[X] * 0x05;
+                break;
+
+            // LD B, VX
+            case 0x33:
+                RAM[I] = (V[X] / 100) % 10;
+                RAM[I + 1] = (V[X] / 10) % 10;
+                RAM[I + 2] = V[X] % 10;
+                break;
+
+            // LD [I], VX
+            case 0x55:
+                for (int r = 0; r <= X; r++)
+                {
+                    RAM[I + r] = V[r];
+                }
+
+                break;
+
+            // LD VX, [I]
+            case 0x65:
+                for (int r = 0; r <= X; r++)
+                {
+                    V[r] = RAM[I + r];
+                }
+
+                break;
+
+            default:
+                break;
+            }
+
+            break;
+
+        default:
             break;
         }
 
