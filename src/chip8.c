652df5b1b08ad58be186d3c53f92195905b3a339
Kurtis Dinelle <kurtisdinelle@gmail.com>
2021-10-30 19:31:17 -0700

    Add more comments

--- src/chip8.c
+++ src/chip8.c
@@ -163,192 +163,221 @@ bool chip8_load_rom(CHIP8 *chip8, char *filename)
 
 void chip8_execute(CHIP8 *chip8)
 {
+    /* Fetch */
     // The first and second byte of instruction respectively.
     unsigned char b1 = chip8->RAM[chip8->PC], b2 = chip8->RAM[chip8->PC + 1];
 
+    /* Decode */
     // The code (first 4 bits) of instruction.
-    unsigned char C = b1 >> 4;
+    unsigned char c = b1 >> 4;
 
-    // The last 4 bits of first byte of instruction.
-    unsigned char X = b1 & 0xF;
-
-    // The first 4 bits of second byte of instruction.
-    unsigned char Y = b2 >> 4;
-
-    // The last 12 bits of instruction (usually an address).
-    unsigned int NNN = ((b1 & 0xF) << 8) | b2;
-
-    // The last 8 bits of instruction.
-    unsigned char NN = b2;
+    // The last 12 bits of instruction.
+    unsigned int nnn = ((b1 & 0xF) << 8) | b2;
 
     // The last 4 bits of instruction.
-    unsigned char N = b2 & 0xF;
+    unsigned char n = b2 & 0xF;
 
+    // The last 4 bits of first byte of instruction.
+    unsigned char x = b1 & 0xF;
+
+    // The first 4 bits of second byte of instruction.
+    unsigned char y = b2 >> 4;
+
+    // The last 8 bits of instruction.
+    unsigned char kk = b2;
+
+    /* Immediately set PC to next instruction
+    after fetching and decoding the current one. */
     chip8->PC += 2;
-
     chip8->display_updated = false;
 
-    // CLS (00E0):
-    if (b1 == 0x00 && b2 == 0xE0)
+    /* Execute */
+    switch (c)
     {
-        chip8_reset_display(chip8);
-    }
+    case 0x00:
+        switch (b2)
+        {
+        /* CLS (00E0)
+           Clear the display. */
+        case 0xE0:
+            chip8_reset_display(chip8);
+            break;
 
-    // RET (00EE):
-    else if (b1 == 0x00 && b2 == 0xEE)
-    {
-        chip8->PC = (chip8->RAM[chip8->SP] << 8) | chip8->RAM[chip8->SP + 1];
-        chip8->SP -= 2;
-    }
+        /* RET (00EE):
+           Return from a subroutine. */
+        case 0xEE:
+            chip8->PC = (chip8->RAM[chip8->SP] << 8);
+            chip8->PC |= chip8->RAM[chip8->SP + 1];
+            chip8->SP -= 2;
+            break;
+        }
 
-    switch (C)
-    {
-    // JMP NNN (1NNN):
-    case 0x01:
-        chip8->PC = NNN;
         break;
 
-    // CALL NNN (2NNN):
+    /* JP addr (1nnn)
+       Jump to location nnn. */
+    case 0x01:
+        chip8->PC = nnn;
+        break;
+
+    /* CALL addr (2nnn)
+       Call subroutine at nnn. */
     case 0x02:
         chip8->SP += 2;
         chip8->RAM[chip8->SP] = chip8->PC >> 8;
         chip8->RAM[chip8->SP + 1] = chip8->PC & 0x00FF;
-        chip8->PC = NNN;
+        chip8->PC = nnn;
         break;
 
-    // SE VX, NN (3XNN):
+    /* SE Vx, byte (3xkk)
+       Skip next instruction if Vx = kk. */
     case 0x03:
-        if (chip8->V[X] == NN)
+        if (chip8->V[x] == kk)
         {
             chip8->PC += 2;
         }
 
         break;
 
-    // SNE VX, NN (4XNN):
+    /* SNE Vx, byte (4xkk)
+       Skip next instruction if Vx != kk. */
     case 0x04:
-        if (chip8->V[X] != NN)
+        if (chip8->V[x] != kk)
         {
             chip8->PC += 2;
         }
 
         break;
 
-    // SE VX, VY (5XY0):
+    /* SE Vx, Vy (5xy0)
+       Skip next instruction if Vx = Vy. */
     case 0x05:
-        if (chip8->V[X] == chip8->V[Y])
+        if (chip8->V[x] == chip8->V[y])
         {
             chip8->PC += 2;
         }
 
         break;
 
-    // LD VX, NN (6XNN):
+    /* LD Vx, byte (6xkk)
+       Set Vx = kk. */
     case 0x06:
-        chip8->V[X] = NN;
+        chip8->V[x] = kk;
         break;
 
-    // ADD VX, NN (7XNN):
+    /* ADD Vx, byte (7xkk)
+       Set Vx = Vx + kk. */
     case 0x07:
-        chip8->V[X] += NN;
+        chip8->V[x] += kk;
         break;
 
-    // Bitwise family
     case 0x08:
-        switch (N)
+        switch (n)
         {
-        // LD VX, VY (8XY0):
+        /* LD Vx, Vy (8xy0)
+           Set Vx = Vy. */
         case 0x00:
-            chip8->V[X] = chip8->V[Y];
+            chip8->V[x] = chip8->V[y];
             break;
 
-        // OR VX, VY (8XY1):
+        /* OR Vx, Vy (8xy1)
+           Set Vx = Vx OR Vy. */
         case 0x01:
-            chip8->V[X] |= chip8->V[Y];
+            chip8->V[x] |= chip8->V[y];
             break;
 
-        // AND VX, VY (8XY2):
+        /* AND Vx, Vy (8xy2)
+           Set Vx = Vx AND Vy. */
         case 0x02:
-            chip8->V[X] &= chip8->V[Y];
+            chip8->V[x] &= chip8->V[y];
             break;
 
-        // XOR VX, VY (8XY3):
+        /* XOR Vx, Vy (8xy3)
+           Set Vx = Vx XOR Vy. */
         case 0x03:
-            chip8->V[X] ^= chip8->V[Y];
+            chip8->V[x] ^= chip8->V[y];
             break;
 
-        // ADD VX, VY (8XY4):
+        /* ADD Vx, Vy (8xy4)
+           Set Vx = Vx + Vy, set VF = carry. */
         case 0x04:
-            chip8->V[0x0F] = ((chip8->V[X] + chip8->V[Y]) > 0xFF) ? 1 : 0;
-            chip8->V[X] += chip8->V[Y];
+            chip8->V[0x0F] = ((chip8->V[x] + chip8->V[y]) > 0xFF) ? 1 : 0;
+            chip8->V[x] += chip8->V[y];
             break;
 
-        // SUB VX, VY (8XY5):
+        /* SUB Vx, Vy (8xy5)
+           Set Vx = Vx - Vy, set VF = NOT borrow. */
         case 0x05:
-            chip8->V[0x0F] = (chip8->V[X] > chip8->V[Y]) ? 1 : 0;
-            chip8->V[X] -= chip8->V[Y];
+            chip8->V[0x0F] = (chip8->V[x] > chip8->V[y]) ? 1 : 0;
+            chip8->V[x] -= chip8->V[y];
             break;
 
-        // SHR VX {, VY} (8XY6):
+        /* SHR Vx {, Vy} (8xy6)
+           Set Vx = Vx SHR 1. */
         case 0x06:
-            //chip8->V[X] = chip8->V[Y];
-            chip8->V[0x0F] = chip8->V[X] & 0x01;
-            chip8->V[X] >>= 1;
+            //chip8->V[x] = chip8->V[y];
+            chip8->V[0x0F] = chip8->V[x] & 0x01;
+            chip8->V[x] >>= 1;
             break;
 
-        // SUBN VX, VY (8XY7):
+        /* SUBN Vx, Vy (8xy7)
+           Set Vx = Vy - Vx, set VF = NOT borrow. */
         case 0x07:
-            chip8->V[0x0F] = (chip8->V[Y] > chip8->V[X]) ? 1 : 0;
-            chip8->V[X] = chip8->V[Y] - chip8->V[X];
+            chip8->V[0x0F] = (chip8->V[y] > chip8->V[x]) ? 1 : 0;
+            chip8->V[x] = chip8->V[y] - chip8->V[x];
             break;
 
-        // SHL VX {, VY} (8XYE):
+        /* SHL Vx {, Vy} (8xyE)
+           Set Vx = Vx SHL 1. */
         case 0x0E:
-            //chip8->V[X] = chip8->V[Y];
-            chip8->V[0x0F] = (chip8->V[X] & 0x80) >> 7;
-            chip8->V[X] <<= 1;
-            break;
-
-        default:
+            //chip8->V[x] = chip8->V[y];
+            chip8->V[0x0F] = (chip8->V[x] & 0x80) >> 7;
+            chip8->V[x] <<= 1;
             break;
         }
 
         break;
 
-    // SNE VX, VY (9XY0):
+    /* SNE Vx, Vy (9xy0)
+       Skip next instruction if Vx != Vy. */
     case 0x09:
-        if (chip8->V[X] != chip8->V[Y])
+        if (chip8->V[x] != chip8->V[y])
         {
             chip8->PC += 2;
         }
 
         break;
 
-    // LD I, NNN (ANNN):
+    /* LD I, addr (Annn)
+       Set I = nnn. */
     case 0x0A:
-        chip8->I = NNN;
+        chip8->I = nnn;
         break;
 
-    // JMP V0, NNN (BNNN):
+    /* JMP V0, addr (Bnnn)
+       Jump to location nnn + V0. */
     case 0x0B:
-        chip8->PC = chip8->V[0] + NNN;
+        chip8->PC = chip8->V[0] + nnn;
         break;
 
-    // RND VX, NN (CXNN):
+    /* RND Vx, byte (Cxkk)
+       Set Vx = random byte AND kk. */
     case 0x0C:
-        chip8->V[X] = (rand() % 256) & NN;
+        chip8->V[x] = (rand() % 256) & kk;
         break;
 
-    // DRW VX, VY, N (DXYN):
+    /* DRW Vx, Vy, n (Dxyn):
+       Display n-byte sprite starting at memory location I at (Vx, Vy),
+       set VF = collision. */
     case 0x0D:
         chip8->V[0x0F] = 0;
 
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < 8; j++)
             {
-                int disp_x = chip8->V[X] + j;
-                int disp_y = chip8->V[Y] + i;
+                int disp_x = chip8->V[x] + j;
+                int disp_y = chip8->V[y] + i;
 
                 // Allow out-of-bound sprite to wrap-around.
                 if (disp_x >= MAX_WIDTH)
@@ -377,44 +406,43 @@ void chip8_execute(CHIP8 *chip8)
 
         break;
 
-    // Key skip family
     case 0x0E:
         switch (b2)
         {
-        // SKP VX (EX9E):
+        /* SKP Vx (Ex9E)
+           Skip next instruction if key with the value of Vx is pressed. */
         case 0x9E:
-            if (chip8->keypad[chip8->V[X]] == KEY_DOWN)
+            if (chip8->keypad[chip8->V[x]] == KEY_DOWN)
             {
                 chip8->PC += 2;
             }
 
             break;
 
-        // SKNP VX (EXA1):
+        /* SKNP Vx (ExA1)
+           Skip next instruction if key with the value of Vx is not pressed. */
         case 0xA1:
-            if (chip8->keypad[chip8->V[X]] == KEY_UP)
+            if (chip8->keypad[chip8->V[x]] == KEY_UP)
             {
                 chip8->PC += 2;
             }
 
-            break;
-
-        default:
             break;
         }
 
         break;
 
-    // F command family
     case 0x0F:
         switch (b2)
         {
-        // LD VX, DT (FX07):
+        /* LD Vx, DT (Fx07)
+           Set Vx = delay timer value. */
         case 0x07:
-            chip8->V[X] = chip8->DT;
+            chip8->V[x] = chip8->DT;
             break;
 
-        // LD VX, K (FX0A):
+        /* LD Vx, K (Fx0A)
+           Wait for a key press, store the value of the key in Vx. */
         case 0x0A:
         {
             bool key_released = false;
@@ -423,7 +451,7 @@ void chip8_execute(CHIP8 *chip8)
             {
                 if (chip8->keypad[i] == KEY_RELEASED)
                 {
-                    chip8->V[X] = i;
+                    chip8->V[x] = i;
                     key_released = true;
                     break;
                 }
@@ -437,58 +465,60 @@ void chip8_execute(CHIP8 *chip8)
             break;
         }
 
-        // LD DT, VX (FX15):
+        /* LD DT, Vx (Fx15)
+           Set delay timer = Vx. */
         case 0x15:
-            chip8->DT = chip8->V[X];
+            chip8->DT = chip8->V[x];
             break;
 
-        // LD ST, VX (FX18):
+        /* LD ST, Vx (Fx18)
+           Set sound timer = Vx. */
         case 0x18:
-            chip8->ST = chip8->V[X];
+            chip8->ST = chip8->V[x];
             break;
 
-        // ADD I, VX (FX1E):
+        /* ADD I, Vx (Fx1E):
+           Set I = I + Vx. */
         case 0x1E:
-            chip8->I += chip8->V[X];
+            chip8->I += chip8->V[x];
             break;
 
-        // LD F, VX (FX29):
+        /* LD F, Vx (Fx29)
+           Set I = location of sprite for digit Vx. */
         case 0x29:
-            chip8->I = chip8->V[X] * 0x05;
+            chip8->I = chip8->V[x] * 0x05;
             break;
 
-        // LD B, VX (FX33):
+        /* LD B, Vx (Fx33)
+           Store BCD representation of Vx in memory locations:
+           I, I+1, and I+2. */
         case 0x33:
-            chip8->RAM[chip8->I] = (chip8->V[X] / 100) % 10;
-            chip8->RAM[chip8->I + 1] = (chip8->V[X] / 10) % 10;
-            chip8->RAM[chip8->I + 2] = chip8->V[X] % 10;
+            chip8->RAM[chip8->I] = (chip8->V[x] / 100) % 10;
+            chip8->RAM[chip8->I + 1] = (chip8->V[x] / 10) % 10;
+            chip8->RAM[chip8->I + 2] = chip8->V[x] % 10;
             break;
 
-        // LD [I], VX (FX55):
+        /* LD [I], Vx (Fx55)
+           Fx55 - LD [I], Vx */
         case 0x55:
-            for (int r = 0; r <= X; r++)
+            for (int r = 0; r <= x; r++)
             {
                 chip8->RAM[chip8->I + r] = chip8->V[r];
             }
 
             break;
 
-        // LD VX, [I] (FX65):
+        /* LD Vx, [I] (Fx65)
+           Read registers V0 through Vx from memory starting at location I. */
         case 0x65:
-            for (int r = 0; r <= X; r++)
+            for (int r = 0; r <= x; r++)
             {
                 chip8->V[r] = chip8->RAM[chip8->I + r];
             }
 
             break;
-
-        default:
-            break;
         }
 
-        break;
-
-    default:
         break;
     }
 
@@ -547,15 +577,6 @@ void chip8_reset_display(CHIP8 *chip8)
     }
 }
 
-void chip8_load_instr(CHIP8 *chip8, unsigned int instr)
-{
-    unsigned char b1 = instr >> 8;
-    unsigned char b2 = instr & 0xFF;
-
-    chip8->RAM[PC_START_ADDR] = b1;
-    chip8->RAM[PC_START_ADDR + 1] = b2;
-}
-
 void chip8_sleep()
 {
 #ifdef WIN32
@@ -568,4 +589,13 @@ void chip8_sleep()
 #else
     usleep(1000000 / CLOCK_SPEED);
 #endif
+}
+
+void chip8_load_instr(CHIP8 *chip8, unsigned int instr)
+{
+    unsigned char b1 = instr >> 8;
+    unsigned char b2 = instr & 0xFF;
+
+    chip8->RAM[PC_START_ADDR] = b1;
+    chip8->RAM[PC_START_ADDR + 1] = b2;
 }
